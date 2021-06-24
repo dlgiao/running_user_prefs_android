6c27a05e56a98e16569b9f160e58ed64f4133f0f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 17:51:16 +0200

    Add UserPrefViewModel.kt

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/UserPrefViewModel.kt
@@ -0,0 +1,7 @@
+package com.kmmania.runninguserpreferences.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.kmmania.runninguserpreferences.repositories.UserPrefRepository
+
+class UserPrefViewModel(private val userPrefRepository: UserPrefRepository): ViewModel() {
+}
