dac83f8a3bfd3bdfbb1f0c590228808d8169f25b
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 16:59:35 +0200

    Add delete() suspend function

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDao.kt
@@ -3,9 +3,14 @@ package com.kmmania.runninguserpreferences.model
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface UserPrefDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(userPref: UserPref)
+
+    @Query("DELETE FROM user_pref_table")
+    suspend fun delete(userPref: UserPref)
+
 }
