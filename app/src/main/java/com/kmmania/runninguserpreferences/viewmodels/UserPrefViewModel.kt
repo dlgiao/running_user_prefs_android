26ea4e3b8721bceb15dd3583d5cf5958793ac0c8
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 17:53:38 +0200

    Add allUserPref val and insert() fun

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/UserPrefViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/UserPrefViewModel.kt
@@ -1,7 +1,17 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.kmmania.runninguserpreferences.model.UserPref
 import com.kmmania.runninguserpreferences.repositories.UserPrefRepository
+import kotlinx.coroutines.launch
 
 class UserPrefViewModel(private val userPrefRepository: UserPrefRepository): ViewModel() {
+    val allUserPref: LiveData<List<UserPref>> = userPrefRepository.allUserPref.asLiveData()
+
+    fun insert(userPref: UserPref) = viewModelScope.launch {
+        userPrefRepository.insert(userPref)
+    }
 }
