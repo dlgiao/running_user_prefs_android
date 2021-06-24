b54ecea80b0ed01ab7d0a2de68689c2a7ea56d89
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 17:06:44 +0200

    Add @Database

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDatabase.kt
@@ -1,4 +1,7 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.Database
+
+@Database(entities = [UserPref::class], version = 1, exportSchema = false)
 abstract class UserPrefDatabase {
 }
