e8e0e4d3e9ad13d7b2102fdf6ef127eb103c4429
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 16:55:19 +0200

    Add insert() suspend function

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDao.kt
@@ -1,7 +1,11 @@
 package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface UserPrefDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(userPref: UserPref)
 }
