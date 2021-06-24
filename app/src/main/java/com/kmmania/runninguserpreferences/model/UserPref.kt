5cef6c3accee5cbce152ea011f826e65de79dad5
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 16:24:25 +0200

    Add mas, unit_mas

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
@@ -4,11 +4,16 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
+import com.kmmania.runninguserpreferences.utils.units.SpeedUnit
 
 @Entity(tableName = "user_pref_table")
 data class UserPref(
     @ColumnInfo(name = "measuring_system")
-    val measuringSystem: MeasuringSystemUnit
+    val measuringSystem: MeasuringSystemUnit,
+    @ColumnInfo(name = "mas")
+    val mas: Double,
+    @ColumnInfo(name = "unit_mas")
+    val unitMas: SpeedUnit
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
