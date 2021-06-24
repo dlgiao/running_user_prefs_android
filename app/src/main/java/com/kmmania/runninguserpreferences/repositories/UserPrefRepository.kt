5cedc6aa75f597386e4b3e3eed3a3a5c7b719b36
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 17:19:05 +0200

    Add UserPrefRepository.kt

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/UserPrefRepository.kt
@@ -0,0 +1,6 @@
+package com.kmmania.runninguserpreferences.repositories
+
+import com.kmmania.runninguserpreferences.model.UserPrefDao
+
+class UserPrefRepository(private val userPrefDao: UserPrefDao) {
+}
