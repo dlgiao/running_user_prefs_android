d5c4d387cc6021186e997a54f94475a255435e6f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 16:20:20 +0200

    Add measuring_system

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
@@ -7,7 +7,7 @@ import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
 
 @Entity(tableName = "user_pref_table")
 data class UserPref(
-
+    @ColumnInfo(name = "measuring_system") val measuringSystem: MeasuringSystemUnit
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
