0fc495656eb5dd468b420ba35f039af07b0396bd
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 17:38:01 +0200

    Add allUserPref val and suspend fun insert

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/UserPrefRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/UserPrefRepository.kt
@@ -1,6 +1,17 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import androidx.annotation.WorkerThread
+import com.kmmania.runninguserpreferences.model.UserPref
 import com.kmmania.runninguserpreferences.model.UserPrefDao
+import kotlinx.coroutines.flow.Flow
 
 class UserPrefRepository(private val userPrefDao: UserPrefDao) {
+
+    val allUserPref: Flow<List<UserPref>> = userPrefDao.getAllUserPref()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(userPref: UserPref) {
+        userPrefDao.insert(userPref)
+    }
 }
