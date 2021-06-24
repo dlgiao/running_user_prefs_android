1e008f52b7ea07b4a5245dc50353e73399efba96
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 16:39:16 +0200

    Add height, unit_height

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
@@ -3,6 +3,7 @@ package com.kmmania.runninguserpreferences.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.kmmania.runninguserpreferences.utils.units.LengthUnit
 import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
 import com.kmmania.runninguserpreferences.utils.units.SpeedUnit
 import java.util.*
@@ -16,7 +17,12 @@ data class UserPref(
     @ColumnInfo(name = "unit_mas")
     val unitMas: SpeedUnit,
     @ColumnInfo(name = "dob")
-    val dob: Date
+    val dob: Date,
+    @ColumnInfo(name = "height")
+    val height: Int,
+    @ColumnInfo(name = "unit_height")
+    val unitHeight: LengthUnit
+
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
