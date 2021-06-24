88ecf30d6b6862ea914d6258b55224a85af3e1ab
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-24 17:08:18 +0200

    Add abstract fun userPrefDao()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefDatabase.kt
@@ -1,7 +1,10 @@
 package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [UserPref::class], version = 1, exportSchema = false)
-abstract class UserPrefDatabase {
+abstract class UserPrefDatabase: RoomDatabase() {
+
+    abstract fun userPrefDao(): UserPrefDao
 }
