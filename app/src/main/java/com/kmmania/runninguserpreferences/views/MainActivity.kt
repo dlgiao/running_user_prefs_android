76be8f95db99f4ac9d295aae85f9c7f75806bd91
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-25 15:57:50 +0200

    Move MainActivity.kt to views package

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kmmania.runninguserpreferences
+package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kmmania.runninguserpreferences.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
