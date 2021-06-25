c42813e26ad82168d8209356cb3dea77e6d20fc8
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-25 17:47:39 +0200

    Create repository and database instances

--- app/src/main/java/com/kmmania/runninguserpreferences/UserPrefApplication.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/UserPrefApplication.kt
@@ -1,4 +1,10 @@
 package com.kmmania.runninguserpreferences
 
-class UserPrefApplication {
+import android.app.Application
+import com.kmmania.runninguserpreferences.model.UserPrefDatabase
+import com.kmmania.runninguserpreferences.repositories.UserPrefRepository
+
+class UserPrefApplication: Application() {
+    private val database by lazy { UserPrefDatabase.getDatabase(this) }
+    val repository by lazy { UserPrefRepository(database.userPrefDao()) }
 }
