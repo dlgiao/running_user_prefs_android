1e77455ed5ffeb6df8ddc4dcff264942eb631993
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-25 18:11:26 +0200

    Add observer who observes LiveData in ViewModel

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -2,11 +2,23 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.UserPrefApplication
+import com.kmmania.runninguserpreferences.viewmodels.UserPrefViewModel
+import com.kmmania.runninguserpreferences.viewmodels.UserPrefViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private val userPrefViewModel: UserPrefViewModel by viewModels {
+        UserPrefViewModelFactory((application as UserPrefApplication).repository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userPrefViewModel.allUserPref.observe(this, { userPrefs ->
+            //
+        })
     }
 }
