f11532ef03aa2aeadc01a8ddbb1d74103c242bb1
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 12:46:56 +0200

    Add database, repository val

--- app/src/main/java/com/kmmania/runninguserpreferences/RunningUserPrefApplication.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/RunningUserPrefApplication.kt
@@ -2,9 +2,14 @@ package com.kmmania.runninguserpreferences
 
 import android.app.Application
 import com.kmmania.runninguserpreferences.model.MeasuringSystemDatabase
+import com.kmmania.runninguserpreferences.model.UserPrefDatabase
 import com.kmmania.runninguserpreferences.repositories.MeasuringSystemRepository
+import com.kmmania.runninguserpreferences.repositories.UserPrefRepository
 
 class RunningUserPrefApplication: Application() {
+    private val database by lazy { UserPrefDatabase.getDatabase(this) }
+    val repository by lazy { UserPrefRepository(database.userPrefDao()) }
+
     private val measuringSystemDatabase by lazy {
         MeasuringSystemDatabase.getDatabase(this)
     }
