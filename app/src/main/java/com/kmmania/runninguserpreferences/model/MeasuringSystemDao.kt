b968f391f98eab62a3e04c3f8afd9f2f962ab40c
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 11:20:50 +0200

    Add get fun

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 interface MeasuringSystemDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -14,4 +15,10 @@ interface MeasuringSystemDao {
 
     @Query("DELETE FROM measuring_system_table")
     suspend fun deleteAll()
+
+    @Query("SELECT * FROM measuring_system_table ORDER BY created_at ASC")
+    fun getAllMeasuringSystem(): Flow<List<MeasuringSystem>>
+
+    @Query("SELECT * FROM measuring_system_table WHERE id = (SELECT max(ID) FROM measuring_system_table)")
+    fun getLastMeasuringSystem(): Flow<MeasuringSystem>
 }
