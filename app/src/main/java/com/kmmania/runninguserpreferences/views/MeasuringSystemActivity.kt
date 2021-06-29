f400f90629b74cd7a0d27b52ebb0fd027f32d057
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 12:17:37 +0200

    Add Measuring system activity

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MeasuringSystemActivity.kt
@@ -0,0 +1,12 @@
+package com.kmmania.runninguserpreferences.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kmmania.runninguserpreferences.R
+
+class MeasuringSystemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_measuring_system)
+    }
+}
