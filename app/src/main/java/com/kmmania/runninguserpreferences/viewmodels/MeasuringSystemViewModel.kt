a7c77476bcb8e59dfc9f34c6de3df78a4448a43e
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 11:54:40 +0200

    Add data

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MeasuringSystemViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MeasuringSystemViewModel.kt
@@ -1,4 +1,30 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
-class MeasuringSystemViewModel {
+import androidx.lifecycle.*
+import com.kmmania.runninguserpreferences.model.MeasuringSystem
+import com.kmmania.runninguserpreferences.repositories.MeasuringSystemRepository
+import kotlinx.coroutines.launch
+
+class MeasuringSystemViewModel(
+    private val measuringSystemRepository: MeasuringSystemRepository
+): ViewModel() {
+    val lastMeasuringSystem: LiveData<MeasuringSystem> = measuringSystemRepository
+        .lastMeasuringSystem
+        .asLiveData()
+
+    fun insert(measuringSystem: MeasuringSystem) = viewModelScope.launch {
+        measuringSystemRepository.insert(measuringSystem)
+    }
+}
+
+class MeasuringSystemViewModelFactory(
+    private val measuringSystemRepository: MeasuringSystemRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MeasuringSystemViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MeasuringSystemViewModel(measuringSystemRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
