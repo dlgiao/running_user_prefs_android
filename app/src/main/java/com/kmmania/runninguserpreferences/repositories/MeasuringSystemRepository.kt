6b7a3e4c3337dc84a733a5838f53f3b95d13ea91
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 11:40:34 +0200

    Add data

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/MeasuringSystemRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/MeasuringSystemRepository.kt
@@ -1,4 +1,17 @@
 package com.kmmania.runninguserpreferences.repositories
 
-class MeasuringSystemRepository {
+import androidx.annotation.WorkerThread
+import com.kmmania.runninguserpreferences.model.MeasuringSystem
+import com.kmmania.runninguserpreferences.model.MeasuringSystemDao
+import kotlinx.coroutines.flow.Flow
+
+class MeasuringSystemRepository(private val measuringSystemDao: MeasuringSystemDao) {
+
+    val lastMeasuringSystem: Flow<MeasuringSystem> = measuringSystemDao.getLastMeasuringSystem()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(measuringSystem: MeasuringSystem) {
+        measuringSystemDao.insert(measuringSystem)
+    }
 }
