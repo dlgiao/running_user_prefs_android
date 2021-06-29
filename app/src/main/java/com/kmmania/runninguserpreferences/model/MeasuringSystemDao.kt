4ad34d0e2a803f61383a3f04ee794a7612548b82
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 11:04:32 +0200

    Add insert()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
@@ -1,4 +1,9 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
 interface MeasuringSystemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(measuringSystem: MeasuringSystem)
 }
