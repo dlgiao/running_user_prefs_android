5ad001ef7f7d7909fa9b1f7cbac94ff65ef12faa
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 12:38:55 +0200

    Add measuringSystemViewModel

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MeasuringSystemActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MeasuringSystemActivity.kt
@@ -2,9 +2,19 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.RunningUserPrefApplication
+import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModel
+import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModelFactory
 
 class MeasuringSystemActivity : AppCompatActivity() {
+    private val measuringSystemViewModel: MeasuringSystemViewModel by viewModels {
+        MeasuringSystemViewModelFactory((application as RunningUserPrefApplication)
+            .measuringSystemRepository
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_measuring_system)
