471e5145ac50f1ecfc284c51927fd4090417186c
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-30 00:12:55 +0200

    Remove AppContainer.kt

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/modules/AppContainer.kt
@@ -0,0 +1,14 @@
+package com.kmmania.runninguserpreferences.modules
+
+import com.kmmania.runninguserpreferences.model.MeasuringSystemDatabase
+import com.kmmania.runninguserpreferences.repositories.MeasuringSystemRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class AppContainer {
+//    private val applicationScope = CoroutineScope(SupervisorJob())
+//    private val msDatabase by lazy {
+//        MeasuringSystemDatabase.getDatabase(this, applicationScope)
+//    }
+//    val msRepository by lazy { MeasuringSystemRepository(msDatabase.measuringSystemDao()) }
+}
