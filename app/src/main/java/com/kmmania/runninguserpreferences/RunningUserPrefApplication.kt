18e72d8e1094d2ee908c26c1928fe3147b791917
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 18:08:22 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/RunningUserPrefApplication.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/RunningUserPrefApplication.kt
@@ -5,13 +5,17 @@ import com.kmmania.runninguserpreferences.model.MeasuringSystemDatabase
 import com.kmmania.runninguserpreferences.model.UserPrefDatabase
 import com.kmmania.runninguserpreferences.repositories.MeasuringSystemRepository
 import com.kmmania.runninguserpreferences.repositories.UserPrefRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class RunningUserPrefApplication: Application() {
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
     private val database by lazy { UserPrefDatabase.getDatabase(this) }
     val repository by lazy { UserPrefRepository(database.userPrefDao()) }
 
     private val measuringSystemDatabase by lazy {
-        MeasuringSystemDatabase.getDatabase(this)
+        MeasuringSystemDatabase.getDatabase(this, applicationScope)
     }
     val measuringSystemRepository by lazy {
         MeasuringSystemRepository(measuringSystemDatabase.measuringSystemDao())
