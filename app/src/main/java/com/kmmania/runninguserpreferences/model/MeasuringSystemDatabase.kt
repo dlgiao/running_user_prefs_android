5e3e3f87abe744ccecfa343251d422d9bd585327
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 18:07:55 +0200

    Add initial value

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDatabase.kt
@@ -1,14 +1,34 @@
 package com.kmmania.runninguserpreferences.model
 
-import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.kmmania.runninguserpreferences.RunningUserPrefApplication
+import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [MeasuringSystem::class], version = 1, exportSchema = false)
 abstract class MeasuringSystemDatabase: RoomDatabase() {
 
     abstract  fun measuringSystemDao(): MeasuringSystemDao
+
+    private class MeasuringSystemDatabaseCallback(
+        private val scope: CoroutineScope
+    ): RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    val measuringSystemDao = database.measuringSystemDao()
+                    measuringSystemDao.deleteAll()
+                    val initialMeasuringSystem = MeasuringSystem(MeasuringSystemUnit.METRIC)
+                    measuringSystemDao.insert(initialMeasuringSystem)
+                }
+            }
+        }
+    }
 
     companion object {
         // Singleton prevents multiple instances of database opening at the
@@ -16,7 +36,10 @@ abstract class MeasuringSystemDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: MeasuringSystemDatabase? = null
 
-        fun getDatabase(context: Context): MeasuringSystemDatabase {
+        fun getDatabase(
+            context: RunningUserPrefApplication,
+            scope: CoroutineScope
+        ): MeasuringSystemDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
@@ -24,7 +47,9 @@ abstract class MeasuringSystemDatabase: RoomDatabase() {
                     context.applicationContext,
                     MeasuringSystemDatabase::class.java,
                     "measuring_system_database"
-                ).build()
+                )
+                    .addCallback(MeasuringSystemDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 // return instance
                 instance
