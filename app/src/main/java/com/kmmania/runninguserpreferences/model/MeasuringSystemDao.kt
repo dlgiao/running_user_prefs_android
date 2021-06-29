9fdad8d37d382564978aa040db7811a422e73cec
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 11:09:08 +0200

    Add delete(), deleteAll()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
@@ -1,9 +1,17 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 interface MeasuringSystemDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(measuringSystem: MeasuringSystem)
+
+    @Delete
+    suspend fun delete(measuringSystem: MeasuringSystem)
+
+    @Query("DELETE FROM measuring_system_table")
+    suspend fun deleteAll()
 }
