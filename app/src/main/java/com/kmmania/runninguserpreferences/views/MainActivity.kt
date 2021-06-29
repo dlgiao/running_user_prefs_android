2bcc2439b397c57ceb02e61bc213003f95420ad5
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 12:38:40 +0200

    Remove userPrefViewModel

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -5,15 +5,16 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.RunningUserPrefApplication
 import com.kmmania.runninguserpreferences.UserPrefApplication
 import com.kmmania.runninguserpreferences.databinding.ActivityMainBinding
 import com.kmmania.runninguserpreferences.viewmodels.UserPrefViewModel
 import com.kmmania.runninguserpreferences.viewmodels.UserPrefViewModelFactory
 
 class MainActivity : AppCompatActivity() {
-    private val userPrefViewModel: UserPrefViewModel by viewModels {
-        UserPrefViewModelFactory((application as UserPrefApplication).repository)
-    }
+//    private val userPrefViewModel: UserPrefViewModel by viewModels {
+//        UserPrefViewModelFactory((application as RunningUserPrefApplication).repository)
+//    }
     private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
