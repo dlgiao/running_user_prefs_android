387453237dfe7af780c86994da4d6ee12743b654
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 11:28:06 +0200

    Add @Database annotation

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDatabase.kt
@@ -1,4 +1,7 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.Database
+
+@Database(entities = [MeasuringSystem::class], version = 1, exportSchema = false)
 abstract class MeasuringSystemDatabase {
 }
