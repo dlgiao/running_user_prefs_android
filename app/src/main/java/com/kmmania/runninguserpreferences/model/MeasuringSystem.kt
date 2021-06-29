5bf2ca99141823edd8faeda7a74fec07948df8e9
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 12:15:01 +0200

    Remove createdAt val

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystem.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystem.kt
@@ -4,15 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
-import java.util.*
 
 @Entity(tableName = "measuring_system_table")
 data class MeasuringSystem(
     @ColumnInfo(name = "measuring_system")
-    val measuringSystem: MeasuringSystemUnit,
-    @ColumnInfo(name = "created_at")
-    val createdAt: Date
+    val measuringSystem: MeasuringSystemUnit
 ) {
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0
+    var id: Long = 0
 }
