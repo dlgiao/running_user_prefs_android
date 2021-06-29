44bc478cff67ac5e3a1fd2d650f97229a3a93e22
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 22:39:20 +0200

    Remove id, change primary key to measuring_system

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystem.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystem.kt
@@ -7,9 +7,11 @@ import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
 
 @Entity(tableName = "measuring_system_table")
 data class MeasuringSystem(
+    @PrimaryKey
     @ColumnInfo(name = "measuring_system")
     val measuringSystem: MeasuringSystemUnit
-) {
-    @PrimaryKey(autoGenerate = true)
-    var id: Long = 0
-}
+)
+//{
+//    @PrimaryKey(autoGenerate = true)
+//    var id: Long = 0
+//}
