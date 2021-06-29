5403230888e61923aa7f39f50011fe3de70e3755
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 22:40:24 +0200

    Remove delete(), getAllMeasuringSystem(), getLastMeasuringSystem(), add getMeasuringSystem()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDao.kt
@@ -8,15 +8,18 @@ interface MeasuringSystemDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(measuringSystem: MeasuringSystem)
 
-    @Delete
-    suspend fun delete(measuringSystem: MeasuringSystem)
+//    @Delete
+//    suspend fun delete(measuringSystem: MeasuringSystem)
 
     @Query("DELETE FROM measuring_system_table")
     suspend fun deleteAll()
 
-    @Query("SELECT * FROM measuring_system_table ORDER BY id ASC")
-    fun getAllMeasuringSystem(): Flow<List<MeasuringSystem>>
+    @Query("SELECT * FROM measuring_system_table")
+    fun getMeasuringSystem(): Flow<MeasuringSystem>
 
-    @Query("SELECT * FROM measuring_system_table WHERE id = (SELECT max(ID) FROM measuring_system_table)")
-    fun getLastMeasuringSystem(): Flow<MeasuringSystem>
+//    @Query("SELECT * FROM measuring_system_table ORDER BY id ASC")
+//    fun getAllMeasuringSystem(): Flow<List<MeasuringSystem>>
+//
+//    @Query("SELECT * FROM measuring_system_table WHERE id = (SELECT max(ID) FROM measuring_system_table)")
+//    fun getLastMeasuringSystem(): Flow<MeasuringSystem>
 }
