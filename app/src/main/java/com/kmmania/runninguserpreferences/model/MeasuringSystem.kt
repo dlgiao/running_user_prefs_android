06a34d9664b770edbab07b624430191838f4d4c8
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 10:59:54 +0200

    Add data

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystem.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystem.kt
@@ -1,6 +1,18 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
+import java.util.*
 
 @Entity(tableName = "measuring_system_table")
-data class MeasuringSystem()
+data class MeasuringSystem(
+    @ColumnInfo(name = "measuring_system")
+    val measuringSystem: MeasuringSystemUnit,
+    @ColumnInfo(name = "created_at")
+    val createdAt: Date
+) {
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0
+}
