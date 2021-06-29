e73adf7f9eca749e093008d3edc27a519ad9b464
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-29 12:48:13 +0200

    Add userPrefViewModel

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -3,18 +3,15 @@ package com.kmmania.runninguserpreferences.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import com.kmmania.runninguserpreferences.R
 import com.kmmania.runninguserpreferences.RunningUserPrefApplication
-import com.kmmania.runninguserpreferences.UserPrefApplication
 import com.kmmania.runninguserpreferences.databinding.ActivityMainBinding
 import com.kmmania.runninguserpreferences.viewmodels.UserPrefViewModel
 import com.kmmania.runninguserpreferences.viewmodels.UserPrefViewModelFactory
 
 class MainActivity : AppCompatActivity() {
-//    private val userPrefViewModel: UserPrefViewModel by viewModels {
-//        UserPrefViewModelFactory((application as RunningUserPrefApplication).repository)
-//    }
+    private val userPrefViewModel: UserPrefViewModel by viewModels {
+        UserPrefViewModelFactory((application as RunningUserPrefApplication).repository)
+    }
     private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
