59763a660e8e7443532733230a841059906238d5
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-30 15:04:09 +0200

    Add onActivityResult

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kmmania.runninguserpreferences.views
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import androidx.activity.viewModels
 import com.kmmania.runninguserpreferences.R
 import com.kmmania.runninguserpreferences.RunningUserPrefApplication
 import com.kmmania.runninguserpreferences.databinding.ActivityMainBinding
+import com.kmmania.runninguserpreferences.model.MeasuringSystem
+import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
 import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModel
 import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModelFactory
 
@@ -17,25 +20,47 @@ class MainActivity : AppCompatActivity() {
         )
     }
     private lateinit var mainBinding: ActivityMainBinding
+    private val msActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
         mainBinding.tvMsTitle.setOnClickListener {
-            val intent = Intent(this, MeasuringSystemActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this@MainActivity, MeasuringSystemActivity::class.java)
+            startActivityForResult(intent, msActivityRequestCode)
         }
 
         msViewModel.lastMeasuringSystem.observe(this, { last ->
             last?.let {
-                var msValue = ""
                 when(it.measuringSystem.toString()) {
-                    "METRIC" -> msValue = getString(R.string.metric)
-                    "IMPERIAL" -> msValue = getString(R.string.imperial)
+                    "METRIC" -> mainBinding.tvMsValue.text = getString(R.string.metric)
+                    "IMPERIAL" -> mainBinding.tvMsValue.text = getString(R.string.imperial)
                 }
-                mainBinding.tvMsValue.text = msValue
             }
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == msActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(MeasuringSystemActivity.EXTRA_REPLY)?.let {
+                when(it) {
+                    "metric" -> {
+                        val metricMS = MeasuringSystem(MeasuringSystemUnit.METRIC)
+                        msViewModel.insert(metricMS)
+                        mainBinding.tvMsTitle.text = it
+                    }
+                    "imperial" -> {
+                        val imperialMS = MeasuringSystem(MeasuringSystemUnit.IMPERIAL)
+                        msViewModel.insert(imperialMS)
+                        mainBinding.tvMsTitle.text = it
+                    }
+                    else -> ""
+                }
+            }
+        }
+
     }
 }
