dee1f6380dadf1a1f1cd0bfeaba07c76701c0e67
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-30 21:39:40 +0200

    Add @Singleton annotation

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/MeasuringSystemRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/MeasuringSystemRepository.kt
@@ -4,7 +4,9 @@ import androidx.annotation.WorkerThread
 import com.kmmania.runninguserpreferences.model.MeasuringSystem
 import com.kmmania.runninguserpreferences.model.MeasuringSystemDao
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Singleton
 
+@Singleton
 class MeasuringSystemRepository(private val msDao: MeasuringSystemDao) {
 
     val msValue: Flow<MeasuringSystem> = msDao.getMeasuringSystem()
