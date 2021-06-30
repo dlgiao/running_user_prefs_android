6162f66358300f2893e45570c883e4748d1ed4c8
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-30 21:54:37 +0200

    Add provideMeasuringSystemDatabase

--- app/src/main/java/com/kmmania/runninguserpreferences/di/DatabaseModule.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/di/DatabaseModule.kt
@@ -1,10 +1,32 @@
 package com.kmmania.runninguserpreferences.di
 
+import android.content.Context
+import androidx.room.Room
+import com.kmmania.runninguserpreferences.model.MeasuringSystemDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideMeasuringSystemDatabase(
+        @ApplicationContext AppContext: Context, scope: CoroutineScope
+    ): MeasuringSystemDatabase {
+        return Room.databaseBuilder(
+            AppContext.applicationContext,
+            MeasuringSystemDatabase::class.java,
+            "measuring_system_database"
+        )
+            .addCallback(MeasuringSystemDatabase.MeasuringSystemDatabaseCallback(scope))
+            .build()
+    }
+
+
 }
