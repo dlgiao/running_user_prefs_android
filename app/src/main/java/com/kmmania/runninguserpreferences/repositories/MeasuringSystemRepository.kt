bc8529436d33c4e4370285c291c5465f7222c4b9
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-30 15:07:55 +0200

    Update msDao val

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/MeasuringSystemRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/MeasuringSystemRepository.kt
@@ -5,13 +5,13 @@ import com.kmmania.runninguserpreferences.model.MeasuringSystem
 import com.kmmania.runninguserpreferences.model.MeasuringSystemDao
 import kotlinx.coroutines.flow.Flow
 
-class MeasuringSystemRepository(private val measuringSystemDao: MeasuringSystemDao) {
+class MeasuringSystemRepository(private val msDao: MeasuringSystemDao) {
 
-    val lastMeasuringSystem: Flow<MeasuringSystem> = measuringSystemDao.getMeasuringSystem()
+    val lastMeasuringSystem: Flow<MeasuringSystem> = msDao.getMeasuringSystem()
 
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
     suspend fun insert(measuringSystem: MeasuringSystem) {
-        measuringSystemDao.insert(measuringSystem)
+        msDao.insert(measuringSystem)
     }
 }
