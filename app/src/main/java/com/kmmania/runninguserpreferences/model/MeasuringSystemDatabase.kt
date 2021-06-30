74c3e4b7cb27fa6b789fc02264cb750c39e76fce
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-06-30 23:34:33 +0200

    Update MeasuringSystemDatabaseCallback

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MeasuringSystemDatabase.kt
@@ -3,32 +3,35 @@ package com.kmmania.runninguserpreferences.model
 import androidx.room.Database
 //import androidx.room.Room
 import androidx.room.RoomDatabase
-//import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 //import com.kmmania.runninguserpreferences.application.RunningUserPrefApplication
-//import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.launch
+import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [MeasuringSystem::class], version = 1, exportSchema = false)
 abstract class MeasuringSystemDatabase: RoomDatabase() {
 
     abstract  fun measuringSystemDao(): MeasuringSystemDao
 
-//    private class MeasuringSystemDatabaseCallback(
-//        private val scope: CoroutineScope
-//    ): RoomDatabase.Callback() {
-//        override fun onCreate(db: SupportSQLiteDatabase) {
-//            super.onCreate(db)
-//            INSTANCE?.let { database ->
-//                scope.launch {
-//                    val measuringSystemDao = database.measuringSystemDao()
-//                    measuringSystemDao.deleteAll()
-//                    val initialMeasuringSystem = MeasuringSystem(MeasuringSystemUnit.METRIC)
-//                    measuringSystemDao.insert(initialMeasuringSystem)
-//                }
-//            }
-//        }
-//    }
+    class MeasuringSystemDatabaseCallback(
+        private val scope: CoroutineScope
+    ): RoomDatabase.Callback() {
+        @Volatile
+        private var INSTANCE: MeasuringSystemDatabase? = null
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            INSTANCE?.let { database ->
+                scope.launch {
+                    val measuringSystemDao = database.measuringSystemDao()
+                    measuringSystemDao.deleteAll()
+                    val initialMeasuringSystem = MeasuringSystem(MeasuringSystemUnit.METRIC)
+                    measuringSystemDao.insert(initialMeasuringSystem)
+                }
+            }
+        }
+    }
 
 //    companion object {
 //        // Singleton prevents multiple instances of database opening at the
