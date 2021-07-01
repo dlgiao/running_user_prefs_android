88069bf237979056a47f1337fe33bffcca7034df
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:03:05 +0200

    Add gender val

--- app/src/main/java/com/kmmania/runninguserpreferences/model/Gender.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/Gender.kt
@@ -1,6 +1,13 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kmmania.runninguserpreferences.utils.units.GenderUnit
 
 @Entity(tableName = "gender_table")
-data class Gender()
+data class Gender(
+    @PrimaryKey
+    @ColumnInfo(name = "gender")
+    val gender: GenderUnit
+)
