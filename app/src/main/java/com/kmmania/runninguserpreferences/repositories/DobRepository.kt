6a105410644ef31ef8575d7838a58fc03c6634c5
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 16:53:32 +0200

    Add @Inject constructor

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/DobRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/DobRepository.kt
@@ -1,7 +1,9 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import com.kmmania.runninguserpreferences.model.DobDao
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DobRepository {
+class DobRepository @Inject constructor(private val dobDao: DobDao) {
 }
