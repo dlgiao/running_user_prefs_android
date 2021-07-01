10fcf32f016a8b806b461274168817530e50595f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:38:19 +0200

    Add fun insert()

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/GenderViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/GenderViewModel.kt
@@ -3,9 +3,11 @@ package com.kmmania.runninguserpreferences.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.kmmania.runninguserpreferences.model.Gender
 import com.kmmania.runninguserpreferences.repositories.GenderRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +15,8 @@ class GenderViewModel @Inject constructor(
     private val genderRepository: GenderRepository
 ): ViewModel() {
     val genderValue: LiveData<Gender> = genderRepository.genderValue.asLiveData()
+
+    fun insert(gender: Gender) = viewModelScope.launch {
+        genderRepository.insert(gender)
+    }
 }
