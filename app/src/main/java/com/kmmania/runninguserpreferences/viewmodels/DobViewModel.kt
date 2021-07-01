ad618df2a1e1397085f939ed70a60e0c981ff1bc
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 17:08:21 +0200

    Add dob val

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/DobViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/DobViewModel.kt
@@ -1,8 +1,16 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.kmmania.runninguserpreferences.model.Dob
+import com.kmmania.runninguserpreferences.repositories.DobRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class DobViewModel: ViewModel() {
+class DobViewModel @Inject constructor(
+    private val dobRepository: DobRepository
+): ViewModel() {
+    val dobValue: LiveData<Dob> = dobRepository.dobValue.asLiveData()
 }
