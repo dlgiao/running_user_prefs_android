4948e2efd522f4f5f88758d81404e5dcecfb2c4a
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:17:22 +0200

    Add insert()

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/GenderRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/GenderRepository.kt
@@ -1,5 +1,6 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import androidx.annotation.WorkerThread
 import com.kmmania.runninguserpreferences.model.Gender
 import com.kmmania.runninguserpreferences.model.GenderDao
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +10,11 @@ import javax.inject.Singleton
 @Singleton
 class GenderRepository @Inject constructor(private val genderDao: GenderDao) {
     val genderValue: Flow<Gender> = genderDao.getGender()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(gender: Gender) {
+        genderDao.deleteAll()
+        genderDao.insert(gender)
+    }
 }
