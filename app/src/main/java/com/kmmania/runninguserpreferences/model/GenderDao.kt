64f888c6534894c87db3e35ffb08ea4185b2ffac
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:06:20 +0200

    Add deleteAll()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/GenderDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/GenderDao.kt
@@ -3,9 +3,13 @@ package com.kmmania.runninguserpreferences.model
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface GenderDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(gender: Gender)
+
+    @Query("DELETE FROM gender_table")
+    suspend fun deleteAll()
 }
