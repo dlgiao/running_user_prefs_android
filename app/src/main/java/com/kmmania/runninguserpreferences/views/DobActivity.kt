082b7ef700f9585f8bc844ffc2a1285a8836fd70
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 18:20:29 +0200

    Add Dob activity

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
@@ -0,0 +1,11 @@
+package com.kmmania.runninguserpreferences.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_dob)
+    }
+}
