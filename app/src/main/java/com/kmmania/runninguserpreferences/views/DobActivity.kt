a0535c5571e592c7fd80bdcd0e6b88103c37524a
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 18:23:58 +0200

    Add dobViewModel val

--- app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
@@ -2,12 +2,15 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.kmmania.runninguserpreferences.databinding.ActivityDobBinding
+import com.kmmania.runninguserpreferences.viewmodels.DobViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DobActivity : AppCompatActivity() {
     private lateinit var dobBinding: ActivityDobBinding
+    private val dobViewModel: DobViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
