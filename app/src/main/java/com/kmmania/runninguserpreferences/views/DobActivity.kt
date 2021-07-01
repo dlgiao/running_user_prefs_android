22e139e20aa1722d70274e7f966e28c4b482b881
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 18:23:10 +0200

    Add dobBinding view binding

--- app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
@@ -2,12 +2,16 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kmmania.runninguserpreferences.databinding.ActivityDobBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DobActivity : AppCompatActivity() {
+    private lateinit var dobBinding: ActivityDobBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_dob)
+        dobBinding = ActivityDobBinding.inflate(layoutInflater)
+        setContentView(dobBinding.root)
     }
 }
