dac76c5dadfa2a97b1597c335da739930583fd25
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 16:54:39 +0200

    Add dobValue val

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/DobRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/DobRepository.kt
@@ -1,9 +1,12 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import com.kmmania.runninguserpreferences.model.Dob
 import com.kmmania.runninguserpreferences.model.DobDao
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DobRepository @Inject constructor(private val dobDao: DobDao) {
+    val dobValue: Flow<Dob> = dobDao.getDob()
 }
