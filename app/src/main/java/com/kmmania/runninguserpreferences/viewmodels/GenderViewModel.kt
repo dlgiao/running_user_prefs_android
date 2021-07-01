8c12a7c9c58e6e39248e23d52732fe91567dafa6
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:20:09 +0200

    Add @Inject constructor, GenderViewModel implements ViewModel()

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/GenderViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/GenderViewModel.kt
@@ -1,7 +1,12 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
+import androidx.lifecycle.ViewModel
+import com.kmmania.runninguserpreferences.repositories.GenderRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class GenderViewModel {
+class GenderViewModel @Inject constructor(
+    private val genderRepository: GenderRepository
+): ViewModel() {
 }
