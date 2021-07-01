ad1fe1b97c1db9661d766a21187443a34db9b29e
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:14:39 +0200

    Add @Inject constructor

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/GenderRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/GenderRepository.kt
@@ -1,7 +1,9 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import com.kmmania.runninguserpreferences.model.GenderDao
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class GenderRepository {
+class GenderRepository @Inject constructor(private val genderDao: GenderDao) {
 }
