7fbb01d79b5a960fa27d755c740cf0f25f5df168
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 17:03:06 +0200

    Add @HiltViewModel annotation

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/DobViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/DobViewModel.kt
@@ -1,6 +1,8 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class DobViewModel: ViewModel() {
 }
