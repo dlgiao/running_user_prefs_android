9af08af7a91dcf2d9fdd3403088bccf61574d45d
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:16:13 +0200

    Add genderValue

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/GenderRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/GenderRepository.kt
@@ -1,9 +1,12 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import com.kmmania.runninguserpreferences.model.Gender
 import com.kmmania.runninguserpreferences.model.GenderDao
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class GenderRepository @Inject constructor(private val genderDao: GenderDao) {
+    val genderValue: Flow<Gender> = genderDao.getGender()
 }
