35f2adcf204f89fda7f3d0d558970b7a0335e484
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 16:42:10 +0200

    Add insert(), deleteAll(), getDob()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/DobDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/DobDao.kt
@@ -1,7 +1,20 @@
 package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import java.util.*
 
 @Dao
 interface DobDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(dob: Date)
+
+    @Query("DELETE FROM dob_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM dob_table")
+    fun getDob(): Flow<Dob>
 }
