fac79a732653ba73eda672760c2a3fba9b95f79c
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 18:21:22 +0200

    Add @AndroidEntryPoint annotation

--- app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
@@ -2,7 +2,9 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DobActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
