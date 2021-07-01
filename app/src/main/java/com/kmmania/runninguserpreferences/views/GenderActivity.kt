3913ad56c8d3bf455c64dd712da3f32b4d4a3778
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 12:55:31 +0200

    Add genderViewModel val

--- app/src/main/java/com/kmmania/runninguserpreferences/views/GenderActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/GenderActivity.kt
@@ -2,11 +2,14 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.kmmania.runninguserpreferences.databinding.ActivityGenderBinding
+import com.kmmania.runninguserpreferences.viewmodels.GenderViewModel
 
 class GenderActivity : AppCompatActivity() {
-
+    private val genderViewModel: GenderViewModel by viewModels()
     private lateinit var genderBinding: ActivityGenderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         genderBinding = ActivityGenderBinding.inflate(layoutInflater)
