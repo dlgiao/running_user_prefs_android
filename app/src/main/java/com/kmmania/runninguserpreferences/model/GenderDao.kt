4ba757f1b275d82f5a331651ff99ef82c64e5779
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:07:53 +0200

    Add getGender()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/GenderDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/GenderDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GenderDao {
@@ -12,4 +13,7 @@ interface GenderDao {
 
     @Query("DELETE FROM gender_table")
     suspend fun deleteAll()
+
+    @Query("SELECT * FROM gender_table")
+    fun getGender(): Flow<Gender>
 }
