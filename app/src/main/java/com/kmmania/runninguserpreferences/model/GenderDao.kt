e6b739fa90a9d46f9d510e8724381bcf799a9443
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:05:37 +0200

    Add insert()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/GenderDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/GenderDao.kt
@@ -1,7 +1,11 @@
 package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface GenderDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(gender: Gender)
 }
