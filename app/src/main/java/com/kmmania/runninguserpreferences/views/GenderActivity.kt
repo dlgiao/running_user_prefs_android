4bf7c7f9c46c5df22f4f7ad0b44830cdbfeeff90
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 12:40:26 +0200

    Add genderBinding view binding

--- app/src/main/java/com/kmmania/runninguserpreferences/views/GenderActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/GenderActivity.kt
@@ -2,10 +2,14 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kmmania.runninguserpreferences.databinding.ActivityGenderBinding
 
 class GenderActivity : AppCompatActivity() {
+
+    private lateinit var genderBinding: ActivityGenderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gender)
+        genderBinding = ActivityGenderBinding.inflate(layoutInflater)
+        setContentView(genderBinding.root)
     }
 }
