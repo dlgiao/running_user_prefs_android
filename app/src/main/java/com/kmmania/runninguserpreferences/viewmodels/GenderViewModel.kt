ab715b7c88df622b7fe1a19a743fc5b0c0048a44
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 10:26:58 +0200

    Add genderValue val

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/GenderViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/GenderViewModel.kt
@@ -1,6 +1,9 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.kmmania.runninguserpreferences.model.Gender
 import com.kmmania.runninguserpreferences.repositories.GenderRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,4 +12,5 @@ import javax.inject.Inject
 class GenderViewModel @Inject constructor(
     private val genderRepository: GenderRepository
 ): ViewModel() {
+    val genderValue: LiveData<Gender> = genderRepository.genderValue.asLiveData()
 }
