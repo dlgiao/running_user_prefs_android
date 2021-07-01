ef6080fa458fe2b38b5b2b556ad0dadfce270409
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-01 16:37:47 +0200

    Add @Entity annotation, dob val

--- app/src/main/java/com/kmmania/runninguserpreferences/model/Dob.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/Dob.kt
@@ -1,3 +1,13 @@
 package com.kmmania.runninguserpreferences.model
 
-data class Dob()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "dob_table")
+data class Dob(
+    @PrimaryKey
+    @ColumnInfo(name = "dob")
+    val dob: Date
+)
