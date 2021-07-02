b75fef4f6da0e3ebf859cd989689170fa90d1c99
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 18:12:30 +0200

    Add masViewModel val

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -17,6 +17,7 @@ import com.kmmania.runninguserpreferences.utils.units.GenderUnit
 import com.kmmania.runninguserpreferences.utils.units.MeasuringSystemUnit
 import com.kmmania.runninguserpreferences.viewmodels.DobViewModel
 import com.kmmania.runninguserpreferences.viewmodels.GenderViewModel
+import com.kmmania.runninguserpreferences.viewmodels.MasViewModel
 import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModel
 //import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     private val msViewModel: MeasuringSystemViewModel by viewModels()
     private val genderViewModel: GenderViewModel by viewModels()
     private val dobViewModel: DobViewModel by viewModels()
+    private val masViewModel: MasViewModel by viewModels()
 
     // StartForResult
     private val msStartForResult = registerForActivityResult(
