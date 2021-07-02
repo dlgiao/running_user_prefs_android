6ef14d558a99ff7bd079e49cc8927ff5fefb3f23
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 17:58:02 +0200

    Add @Inject constructor

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
@@ -1,8 +1,12 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.kmmania.runninguserpreferences.repositories.MasRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MasViewModel: ViewModel() {
+class MasViewModel @Inject constructor(
+    private val masRepository: MasRepository
+): ViewModel() {
 }
