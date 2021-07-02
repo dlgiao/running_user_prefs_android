71e7c519319e22824a508ad355ac3843da138013
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 12:11:46 +0200

    Add @TypeConverters annotation

--- app/src/main/java/com/kmmania/runninguserpreferences/model/DobDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/DobDatabase.kt
@@ -2,8 +2,10 @@ package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Dob::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class DobDatabase: RoomDatabase() {
     abstract fun dobDao(): DobDao
 }
