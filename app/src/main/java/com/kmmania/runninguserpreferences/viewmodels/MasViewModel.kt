35473827b2208c8f0d91ba12bf1b853d4a492449
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 17:58:53 +0200

    Add masValue val

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
@@ -1,6 +1,9 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.kmmania.runninguserpreferences.model.Mas
 import com.kmmania.runninguserpreferences.repositories.MasRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,4 +12,5 @@ import javax.inject.Inject
 class MasViewModel @Inject constructor(
     private val masRepository: MasRepository
 ): ViewModel() {
+    val masValue: LiveData<Mas> = masRepository.masValue.asLiveData()
 }
