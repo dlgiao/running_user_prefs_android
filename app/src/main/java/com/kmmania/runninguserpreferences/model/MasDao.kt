9b296e96a6cffa710664bf18a3a8d111db052d13
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 17:28:49 +0200

    Add insert(), deleteAll(), getMas()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/MasDao.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/MasDao.kt
@@ -1,7 +1,19 @@
 package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MasDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(mas: Mas)
+
+    @Query("DELETE FROM mas_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM mas_table")
+    fun getMas(): Flow<Mas>
 }
