3ac831305349c00689952a45ad10f634aa61ed18
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 17:59:32 +0200

    Add insert()

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
@@ -3,9 +3,11 @@ package com.kmmania.runninguserpreferences.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.kmmania.runninguserpreferences.model.Mas
 import com.kmmania.runninguserpreferences.repositories.MasRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +15,8 @@ class MasViewModel @Inject constructor(
     private val masRepository: MasRepository
 ): ViewModel() {
     val masValue: LiveData<Mas> = masRepository.masValue.asLiveData()
+
+    fun insert(mas: Mas) = viewModelScope.launch {
+        masRepository.insert(mas)
+    }
 }
