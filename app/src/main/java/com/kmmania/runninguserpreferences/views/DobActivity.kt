e7d161ffb1c00123e69eee7f8ab7497d16d24e06
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 13:52:42 +0200

    Add replyIntent()

--- app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/DobActivity.kt
@@ -1,5 +1,7 @@
 package com.kmmania.runninguserpreferences.views
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -20,5 +22,18 @@ class DobActivity : AppCompatActivity() {
         dobViewModel.dobValue.observe(this, { value ->
             //
         })
+    }
+
+    private fun replyIntent(value: String) {
+        val replyIntentValue = Intent()
+        replyIntentValue.putExtra(EXTRA_REPLY, value)
+        setResult(Activity.RESULT_OK, replyIntentValue)
+
+        finish()
+    }
+
+    // TODO: Create CONSTANTS file
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
     }
 }
