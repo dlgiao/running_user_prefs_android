522498e7f0072c6fc5875543929a594371db5cb3
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 18:08:14 +0200

    Add mas activity

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MasActivity.kt
@@ -0,0 +1,11 @@
+package com.kmmania.runninguserpreferences.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_mas)
+    }
+}
