1b471c65516a5ddbf71ac445d408c53be95323ce
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 12:09:14 +0200

    Add fromTimestamp()

--- app/src/main/java/com/kmmania/runninguserpreferences/model/Converters.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/Converters.kt
@@ -1,4 +1,13 @@
 package com.kmmania.runninguserpreferences.model
 
+import androidx.room.TypeConverter
+import java.util.*
+
 class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+
 }
