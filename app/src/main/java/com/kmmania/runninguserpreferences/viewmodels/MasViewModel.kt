abf473f96f5d33403a526edbb1f1eed5181540a2
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 17:57:04 +0200

    Implements ViewModel()

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/MasViewModel.kt
@@ -1,7 +1,8 @@
 package com.kmmania.runninguserpreferences.viewmodels
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
 @HiltViewModel
-class MasViewModel {
+class MasViewModel: ViewModel() {
 }
