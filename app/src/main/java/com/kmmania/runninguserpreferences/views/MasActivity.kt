b0aef9898d33a60c803c1fbddd8a19146f9de13f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-02 18:10:09 +0200

    Add masBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MasActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MasActivity.kt
@@ -2,10 +2,14 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kmmania.runninguserpreferences.databinding.ActivityMasBinding
 
 class MasActivity : AppCompatActivity() {
+    private lateinit var masBinding: ActivityMasBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_mas)
+        masBinding = ActivityMasBinding.inflate(layoutInflater)
+        setContentView(masBinding.root)
     }
 }
