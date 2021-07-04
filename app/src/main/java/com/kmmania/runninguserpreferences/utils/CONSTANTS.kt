c44cc0b1ea0e6ab3f1727041565df0911d85e1b3
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-04 18:34:06 +0200

    Move

--- app/src/main/java/com/kmmania/runninguserpreferences/utils/CONSTANTS.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/utils/CONSTANTS.kt
@@ -1,4 +1,4 @@
-package com.kmmania.runninguserpreferences.utils.units
+package com.kmmania.runninguserpreferences.utils
 
 object CONSTANTS {
     //const val EXTRA_REPLY1 = "com.example.android.wordlistsql.REPLY1"
