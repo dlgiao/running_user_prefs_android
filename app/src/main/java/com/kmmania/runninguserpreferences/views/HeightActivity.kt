4f2ea24838a4d2f35d8d695c86d836726bb515da
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-05 12:27:56 +0200

    Add heightBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/views/HeightActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/HeightActivity.kt
@@ -2,10 +2,14 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kmmania.runninguserpreferences.databinding.ActivityHeightBinding
 
 class HeightActivity : AppCompatActivity() {
+    private lateinit var heightBinding: ActivityHeightBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        heightBinding = ActivityHeightBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_height)
+        setContentView(heightBinding.root)
     }
 }
