90871bd45d30910e3921277b9433aa6c50faeffa
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-05 17:43:27 +0200

    Add weightBinding view binding

--- app/src/main/java/com/kmmania/runninguserpreferences/views/WeightActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/WeightActivity.kt
@@ -2,10 +2,14 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kmmania.runninguserpreferences.databinding.ActivityWeightBinding
 
 class WeightActivity : AppCompatActivity() {
+    private lateinit var weightBinding: ActivityWeightBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_weight)
+        weightBinding = ActivityWeightBinding.inflate(layoutInflater)
+        setContentView(weightBinding.root)
     }
 }
