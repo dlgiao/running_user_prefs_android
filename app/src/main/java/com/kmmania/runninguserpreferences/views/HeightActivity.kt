e60204bf24c23c11e61afae787022ae58d22157b
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-05 16:08:05 +0200

    Add heightViewModel, msViewModel

--- app/src/main/java/com/kmmania/runninguserpreferences/views/HeightActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/HeightActivity.kt
@@ -2,10 +2,15 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.kmmania.runninguserpreferences.databinding.ActivityHeightBinding
+import com.kmmania.runninguserpreferences.viewmodels.HeightViewModel
+import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModel
 
 class HeightActivity : AppCompatActivity() {
     private lateinit var heightBinding: ActivityHeightBinding
+    private val heightViewModel: HeightViewModel by viewModels()
+    private val msViewModel: MeasuringSystemViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         heightBinding = ActivityHeightBinding.inflate(layoutInflater)
