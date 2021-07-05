a44e68e4ffd6d94299e5cf91e01e3c325481d605
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-05 18:10:03 +0200

    Add weightViewModel, msViewModel

--- app/src/main/java/com/kmmania/runninguserpreferences/views/WeightActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/WeightActivity.kt
@@ -2,10 +2,15 @@ package com.kmmania.runninguserpreferences.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.kmmania.runninguserpreferences.databinding.ActivityWeightBinding
+import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModel
+import com.kmmania.runninguserpreferences.viewmodels.WeightViewModel
 
 class WeightActivity : AppCompatActivity() {
     private lateinit var weightBinding: ActivityWeightBinding
+    private val weightViewModel: WeightViewModel by viewModels()
+    private val msViewModel: MeasuringSystemViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
