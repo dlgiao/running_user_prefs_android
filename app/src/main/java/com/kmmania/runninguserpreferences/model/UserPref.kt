53668fe65dbdea7409ee8bee9a2dffa38942b044
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-06 11:30:33 +0200

    Remove comments

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
@@ -7,14 +7,14 @@ import com.kmmania.runninguserpreferences.utils.units.*
 
 @Entity(tableName = "user_pref_table")
 data class UserPref(
-//    @ColumnInfo(name = "measuring_system")
-//    val measuringSystem: MeasuringSystemUnit,
-//    @ColumnInfo(name = "mas")
-//    val mas: Double,
-//    @ColumnInfo(name = "unit_mas")
-//    val unitMas: SpeedUnit,
-//    @ColumnInfo(name = "dob")
-//    val dob: String,
+    @ColumnInfo(name = "measuring_system")
+    val measuringSystem: MeasuringSystemUnit,
+    @ColumnInfo(name = "mas")
+    val mas: Double,
+    @ColumnInfo(name = "unit_mas")
+    val unitMas: SpeedUnit,
+    @ColumnInfo(name = "dob")
+    val dob: String,
     @ColumnInfo(name = "height")
     val height: Int,
     @ColumnInfo(name = "unit_height")
@@ -23,9 +23,8 @@ data class UserPref(
     val weight: Double,
     @ColumnInfo(name = "unit_weight")
     val unitWeightUnit: WeightUnit,
-//    @ColumnInfo(name = "gender")
-//    val gender: Gender
-
+    @ColumnInfo(name = "gender")
+    val gender: Gender
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
