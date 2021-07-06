7ad24062c1dd60d0af384bbeab74ff9719d45c5f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-06 11:42:15 +0200

    Add provideUserPrefDatabase()

--- app/src/main/java/com/kmmania/runninguserpreferences/di/DatabaseModule.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/di/DatabaseModule.kt
@@ -8,13 +8,24 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideUserPrefDatabase(
+        @ApplicationContext AppContext: Context
+    ): UserPrefDatabase {
+        return Room.databaseBuilder(
+            AppContext.applicationContext,
+            UserPrefDatabase::class.java,
+            "user_pref_database"
+        )
+            .build()
+    }
+
     @Provides
     @Singleton
     fun provideMeasuringSystemDatabase(
