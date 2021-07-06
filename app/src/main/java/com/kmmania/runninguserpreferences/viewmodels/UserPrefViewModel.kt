a565f66c08bfe2989926d8f310f715ceafe58827
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-06 11:52:02 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/UserPrefViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/viewmodels/UserPrefViewModel.kt
@@ -3,22 +3,17 @@ package com.kmmania.runninguserpreferences.viewmodels
 import androidx.lifecycle.*
 import com.kmmania.runninguserpreferences.model.UserPref
 import com.kmmania.runninguserpreferences.repositories.UserPrefRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class UserPrefViewModel(private val userPrefRepository: UserPrefRepository): ViewModel() {
-    val allUserPref: LiveData<List<UserPref>> = userPrefRepository.allUserPref.asLiveData()
+@HiltViewModel
+class UserPrefViewModel @Inject constructor(
+    private val userPrefRepository: UserPrefRepository
+): ViewModel() {
+    val userPrefValue: LiveData<UserPref> = userPrefRepository.userPrefValue.asLiveData()
 
     fun insert(userPref: UserPref) = viewModelScope.launch {
         userPrefRepository.insert(userPref)
-    }
-}
-
-class UserPrefViewModelFactory(private val userPrefRepository: UserPrefRepository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(UserPrefViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return UserPrefViewModel(userPrefRepository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
