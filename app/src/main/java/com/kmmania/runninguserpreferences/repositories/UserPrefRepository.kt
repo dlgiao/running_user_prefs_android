7690e70cd3cba36fbc4a7f606a1c5370fe1da73a
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-06 11:46:51 +0200

    Add @Inject constructor

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/UserPrefRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/UserPrefRepository.kt
@@ -4,10 +4,11 @@ import androidx.annotation.WorkerThread
 import com.kmmania.runninguserpreferences.model.UserPref
 import com.kmmania.runninguserpreferences.model.UserPrefDao
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class UserPrefRepository(private val userPrefDao: UserPrefDao) {
+class UserPrefRepository @Inject constructor(private val userPrefDao: UserPrefDao) {
     val userPrefValue: Flow<UserPref> = userPrefDao.getUserPref()
 
     @Suppress("RedundantSuspendModifier")
