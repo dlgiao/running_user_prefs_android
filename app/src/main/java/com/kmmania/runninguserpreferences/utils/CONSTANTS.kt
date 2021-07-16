b5137d223da9315f4300ee8dd94ca2769d0a3f27
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-16 14:49:41 +0200

    Update EXTRA_REPLY_DATE to EXTRA_REPLY_LONG

--- app/src/main/java/com/kmmania/runninguserpreferences/utils/CONSTANTS.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/utils/CONSTANTS.kt
@@ -4,6 +4,6 @@ class CONSTANTS {
     companion object {
         const val EXTRA_REPLY_STRING = "com.kmmania.android.EXTRA_REPLY_STRING"
         const val EXTRA_REPLY_ARRAY = "com.kmmania.android.EXTRA_REPLY_ARRAY"
-        const val EXTRA_REPLY_DATE = "com.kmmania.android.EXTRA_REPLY_DATE"
+        const val EXTRA_REPLY_LONG = "com.kmmania.android.EXTRA_REPLY_LONG"
     }
 }
