2f0857deb185142d73faff39586d98fe893b2be1
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-16 14:09:57 +0200

    Update version to 2

--- app/src/main/java/com/kmmania/runninguserpreferences/model/DobDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/DobDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Dob::class], version = 1, exportSchema = false)
+@Database(entities = [Dob::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class DobDatabase: RoomDatabase() {
     abstract fun dobDao(): DobDao
