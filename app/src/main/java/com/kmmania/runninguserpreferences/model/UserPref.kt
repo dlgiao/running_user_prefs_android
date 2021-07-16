b2b3343fdfa18e6c523ef313ffa37c1b227dd1a4
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-16 22:21:21 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPref.kt
@@ -4,27 +4,29 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.kmmania.runninguserpreferences.utils.units.*
+import java.util.*
 
 @Entity(tableName = "user_pref_table")
 data class UserPref(
     @ColumnInfo(name = "measuring_system")
     val measuringSystem: MeasuringSystemUnit?,
-    @ColumnInfo(name = "mas")
-    val mas: Double?,
-    @ColumnInfo(name = "unit_mas")
-    val unitMas: SpeedUnit?,
-    @ColumnInfo(name = "dob")
-    val dob: String?,
-    @ColumnInfo(name = "height")
-    val height: Int?,
-    @ColumnInfo(name = "unit_height")
-    val unitHeight: LengthUnit?,
-    @ColumnInfo(name = "weight")
-    val weight: Double?,
-    @ColumnInfo(name = "unit_weight")
-    val unitWeightUnit: WeightUnit?,
     @ColumnInfo(name = "gender")
-    val gender: GenderUnit?
+    val gender: GenderUnit?,
+    @ColumnInfo(name = "dob")
+    val dob: Date?,
+    @ColumnInfo(name = "mas_value")
+    val masValue: Double?,
+    @ColumnInfo(name = "mas_unit")
+    val masUnit: SpeedUnit?,
+    @ColumnInfo(name = "height_value")
+    val heightValue: Int?,
+    @ColumnInfo(name = "height_unit")
+    val heightUnit: LengthUnit?,
+    @ColumnInfo(name = "weight_value")
+    val weightValue: Double?,
+    @ColumnInfo(name = "weight_unit")
+    val weightUnit: WeightUnit?
+
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
