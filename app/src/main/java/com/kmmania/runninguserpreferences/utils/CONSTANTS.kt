aed69b886ea5579b3e6dc3d4b2ff90feef2ab8af
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-28 15:14:55 +0200

    Add const

--- app/src/main/java/com/kmmania/runninguserpreferences/utils/CONSTANTS.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/utils/CONSTANTS.kt
@@ -5,5 +5,47 @@ class CONSTANTS {
         const val EXTRA_REPLY_STRING = "com.kmmania.android.EXTRA_REPLY_STRING"
         const val EXTRA_REPLY_ARRAY = "com.kmmania.android.EXTRA_REPLY_ARRAY"
         const val EXTRA_REPLY_LONG = "com.kmmania.android.EXTRA_REPLY_LONG"
+
+        // PACE
+        const val MIN_PACE = 0
+        const val MAX_PACE_MIN = 10
+        const val MAX_PACE_SEC = 59
+
+        // LENGTH
+        const val MIN_DISTANCE = 0.0
+        const val MAX_DISTANCE1 = 10000.0
+        const val MAX_DISTANCE2 = 150.0
+        const val M_KM = 1000
+        const val YD_MI = 1760
+
+        const val MAX_HEIGHT_CM = 250
+        const val MAX_WEIGHT_KG = 150
+
+        // SPEED
+        const val MIN_SPEED = 0.0
+        const val MAX_SPEED = 30.0
+//    const val MAX_SPEED_INT = 25
+//    const val MAX_SPEED_DEC = 9
+
+        //    const val MAX_MAS_INT = 25
+//    const val MAX_MAS_DEC = 9
+        const val MIN_MAS = 0.0
+        const val MAX_MAS = 30.0
+        const val MIN_PC_MAS = 0
+        const val MAX_PC_MAS = 130
+
+        // DURATION
+        const val MIN_DURATION = 0
+        const val MAX_DURATION_H = 100
+        const val MAX_DURATION_MIN = 59
+        const val MAX_DURATION_SEC = 59
+        const val MAX_DURATION_MS = 999
+
+        // WEIGHT
+        const val MAX_HEIGHT_IN = 100
+        const val MAX_WEIGHT_LB = 330
+
+        // AGE
+        const val MAX_AGE = 120
     }
 }
