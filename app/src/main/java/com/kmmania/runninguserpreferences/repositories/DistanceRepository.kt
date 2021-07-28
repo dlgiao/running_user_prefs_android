53b2082d581dd03b9a65f141ef7b9173a6a82d68
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-07-28 15:21:42 +0200

    Add

--- app/src/main/java/com/kmmania/runninguserpreferences/repositories/DistanceRepository.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/repositories/DistanceRepository.kt
@@ -1,4 +1,15 @@
 package com.kmmania.runninguserpreferences.repositories
 
+import com.kmmania.runninguserpreferences.model.DaoInterface
+import com.kmmania.runninguserpreferences.model.Distance
+import com.kmmania.runninguserpreferences.model.units.LengthUnit
+import com.kmmania.runninguserpreferences.model.units.MeasuringSystemUnit
+
 class DistanceRepository {
+    private val distanceValue = 13.6
+    private val distanceUnit: LengthUnit = LengthUnit.KM
+    private val distanceMs: MeasuringSystemUnit = MeasuringSystemUnit.METRIC
+    private val distance: DaoInterface = Distance(distanceValue, distanceUnit, distanceMs)
+
+    val distanceVal: MutableMap<String, Any> = distance.getData()
 }
