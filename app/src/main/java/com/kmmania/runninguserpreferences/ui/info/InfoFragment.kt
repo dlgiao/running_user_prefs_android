de5e810ec7e4da19e371b21302e4e2eb637e93b6
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-12 15:42:26 +0200

    Add infoBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/info/InfoFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/info/InfoFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
+    private var _infoBinding: FragmentInfoBinding? = null
+    private val infoBinding get() = _infoBinding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+
+        _infoBinding = FragmentInfoBinding.inflate(inflater, container, false)
+        val rootView = infoBinding.root
+
+        return rootView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _infoBinding = null
     }
 }
