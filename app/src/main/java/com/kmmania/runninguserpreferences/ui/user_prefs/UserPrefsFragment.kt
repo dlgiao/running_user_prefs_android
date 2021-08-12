4a426e7fe96d0900f8a60d46eec652c818c65176
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-12 15:45:11 +0200

    Add userPrefBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
@@ -6,14 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.databinding.FragmentUserPrefsBinding
 
 class UserPrefsFragment : Fragment() {
+    private var _userPrefsBinding: FragmentUserPrefsBinding? = null
+    private val userPrefsBinding get() = _userPrefsBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_prefs, container, false)
+
+        _userPrefsBinding = FragmentUserPrefsBinding.inflate(inflater, container, false)
+        val rootView = userPrefsBinding.root
+
+        return rootView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _userPrefsBinding = null
     }
 }
