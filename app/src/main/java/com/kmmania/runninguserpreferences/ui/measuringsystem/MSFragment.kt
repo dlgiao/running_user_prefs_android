4ce22249b47c7b9530d8feb0e729f52e7e21d670
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-12 17:43:13 +0200

    Add msBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/measuringsystem/MSFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/measuringsystem/MSFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.databinding.FragmentMSBinding
 
 class MSFragment : Fragment() {
+    private var _msBinding: FragmentMSBinding? = null
+    private val msBinding get() = _msBinding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_m_s, container, false)
+
+        _msBinding = FragmentMSBinding.inflate(inflater, container, false)
+        val rootView = msBinding.root
+
+        return rootView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _msBinding = null
     }
 }
