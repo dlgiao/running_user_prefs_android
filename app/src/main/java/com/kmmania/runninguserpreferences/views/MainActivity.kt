c55b7fba7b2d8a72e8bb50bec06321b374b1baaf
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-12 11:54:34 +0200

    Update navBarConfiguration

--- app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/views/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.kmmania.runninguserpreferences.R
 import com.kmmania.runninguserpreferences.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,5 +23,12 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout = mainBinding.drawerLayout
         val navView = mainBinding.navView
         val navController = findNavController(R.id.nav_host_fragment_container)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_user_prefs, R.id.nav_info),
+            drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
