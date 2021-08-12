c887ac8a158af27ad8fbadf1d52b585f787f5d9e
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-12 18:11:24 +0200

    Add ms view model and observer

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.kmmania.runninguserpreferences.R
 import com.kmmania.runninguserpreferences.databinding.FragmentUserPrefsBinding
+import com.kmmania.runninguserpreferences.model.MeasuringSystem
+import com.kmmania.runninguserpreferences.viewmodels.MeasuringSystemViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserPrefsFragment : Fragment() {
     private var _userPrefsBinding: FragmentUserPrefsBinding? = null
     private val userPrefsBinding get() = _userPrefsBinding!!
+
+    // ViewModel
+    private val msViewModel: MeasuringSystemViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +28,21 @@ class UserPrefsFragment : Fragment() {
 
         _userPrefsBinding = FragmentUserPrefsBinding.inflate(inflater, container, false)
         val rootView = userPrefsBinding.root
+
+        // Measuring system
+
+        // The observer which updates the UI
+        val msObserver = Observer<MeasuringSystem> { ms ->
+            ms?.let {
+                when(it.measuringSystem.toString()) {
+                    "METRIC" -> userPrefsBinding.tvMsValue.text = getString(R.string.metric)
+                    "IMPERIAL" -> userPrefsBinding.tvMsValue.text = getString(R.string.imperial)
+                    else -> userPrefsBinding.tvMsValue.text = getString(R.string.unknown)
+                }
+            }
+        }
+        // Observe the LiveData
+        msViewModel.msValue.observe(viewLifecycleOwner, msObserver)
 
         return rootView
     }
