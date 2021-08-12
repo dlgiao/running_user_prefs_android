cb89c96ebfad47805bd18de89027f6f00f5e20fc
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-12 15:42:16 +0200

    Add homeBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/home/HomeFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _homeBinding: FragmentHomeBinding? = null
+    private val homeBinding get() = _homeBinding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        _homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        val rootView = homeBinding.root
+
+        return rootView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _homeBinding = null
     }
 }
