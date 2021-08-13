e439d908611dd744e617d5b2e6242aa2c618d796
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-13 23:54:18 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
@@ -13,8 +13,8 @@ import com.kmmania.runninguserpreferences.R
 import com.kmmania.runninguserpreferences.databinding.FragmentUserPrefsBinding
 import com.kmmania.runninguserpreferences.model.*
 import com.kmmania.runninguserpreferences.model.units.*
-import com.kmmania.runninguserpreferences.ui.measuringsystem.MeasuringSystemViewModel
-import com.kmmania.runninguserpreferences.viewmodels.*
+//import com.kmmania.runninguserpreferences.ui.measuringsystem.MeasuringSystemViewModel
+//import com.kmmania.runninguserpreferences.viewmodels.*
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
@@ -26,12 +26,12 @@ class UserPrefsFragment : Fragment() {
     // ViewModel
     private val userPrefsViewModel: UserPrefsViewModel by viewModels()
 
-    private val msViewModel: MeasuringSystemViewModel by viewModels()
-    private val genderViewModel: GenderViewModel by viewModels()
-    private val dobViewModel: DobViewModel by viewModels()
-    private val masViewModel: MasViewModel by viewModels()
-    private val heightViewModel: HeightViewModel by viewModels()
-    private val weightViewModel: WeightViewModel by viewModels()
+//    private val msViewModel: MeasuringSystemViewModel by viewModels()
+//    private val genderViewModel: GenderViewModel by viewModels()
+//    private val dobViewModel: DobViewModel by viewModels()
+//    private val masViewModel: MasViewModel by viewModels()
+//    private val heightViewModel: HeightViewModel by viewModels()
+//    private val weightViewModel: WeightViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,7 +44,7 @@ class UserPrefsFragment : Fragment() {
 
         val userPrefsObserver = Observer<UserPrefs> { userPrefs ->
             userPrefs?.let {
-                when (it.measuringSystem.toString()) {
+                when(it.measuringSystem.toString()) {
                     "METRIC" -> {
                         userPrefsBinding.rbMetric.isChecked = true
                         userPrefsBinding.tvUnitMas.text = getString(R.string.kmh)
@@ -96,10 +96,26 @@ class UserPrefsFragment : Fragment() {
 //        msViewModel.msValue.observe(viewLifecycleOwner, msObserver)
         // insert MS data
         userPrefsBinding.rbMetric.setOnClickListener {
-            msViewModel.insert(MeasuringSystem(MeasuringSystemUnit.METRIC))
+            //msViewModel.insert(MeasuringSystem(MeasuringSystemUnit.METRIC))
+            userPrefsViewModel.insert(UserPrefs(
+                MeasuringSystemUnit.METRIC,
+                getGender(),
+                getDob(),
+                getMasValue(), getMasUnit(),
+                getHeightValue(), getHeightUnit(),
+                getWeightValue(), getWeightUnit()
+            ))
         }
         userPrefsBinding.rbImperial.setOnClickListener {
-            msViewModel.insert(MeasuringSystem(MeasuringSystemUnit.IMPERIAL))
+            //msViewModel.insert(MeasuringSystem(MeasuringSystemUnit.IMPERIAL))
+            userPrefsViewModel.insert(UserPrefs(
+                MeasuringSystemUnit.IMPERIAL,
+                getGender(),
+                getDob(),
+                getMasValue(), getMasUnit(),
+                getHeightValue(), getHeightUnit(),
+                getWeightValue(), getWeightUnit()
+            ))
         }
 
         // Gender
@@ -116,10 +132,26 @@ class UserPrefsFragment : Fragment() {
 //        genderViewModel.genderValue.observe(viewLifecycleOwner, genderObserver)
         // Insert gender data
         userPrefsBinding.rbMale.setOnClickListener {
-            genderViewModel.insert(Gender(GenderUnit.MALE))
+            //genderViewModel.insert(Gender(GenderUnit.MALE))
+            userPrefsViewModel.insert(UserPrefs(
+                getMS(),
+                GenderUnit.MALE,
+                getDob(),
+                getMasValue(), getMasUnit(),
+                getHeightValue(), getHeightUnit(),
+                getWeightValue(), getWeightUnit()
+            ))
         }
         userPrefsBinding.rbFemale.setOnClickListener {
-            genderViewModel.insert(Gender(GenderUnit.FEMALE))
+            //genderViewModel.insert(Gender(GenderUnit.FEMALE))
+            userPrefsViewModel.insert(UserPrefs(
+                getMS(),
+                GenderUnit.FEMALE,
+                getDob(),
+                getMasValue(), getMasUnit(),
+                getHeightValue(), getHeightUnit(),
+                getWeightValue(), getWeightUnit()
+            ))
         }
 
         // DOB
@@ -140,8 +172,16 @@ class UserPrefsFragment : Fragment() {
             dobPicker.show(childFragmentManager, "DOB")
         }
         dobPicker.addOnPositiveButtonClickListener {
-            val dobValue = Date(dobPicker.selection!!)
-            dobViewModel.insert(Dob(dobValue))
+            //val dobValue = Date(dobPicker.selection!!)
+            //dobViewModel.insert(Dob(dobValue))
+            userPrefsViewModel.insert(UserPrefs(
+                getMS(),
+                getGender(),
+                Date(dobPicker.selection!!),
+                getMasValue(), getMasUnit(),
+                getHeightValue(), getHeightUnit(),
+                getWeightValue(), getWeightUnit()
+            ))
         }
 
         // MAS
@@ -154,13 +194,21 @@ class UserPrefsFragment : Fragment() {
 
         userPrefsBinding.etMasValue.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                val mas = userPrefsBinding.tiMasValue.editText?.text.toString().toDouble()
-                var unitMas = SpeedUnit.KMH
-                when(userPrefsBinding.tvUnitMas.text.toString()) {
-                    "km/h" -> unitMas = SpeedUnit.KMH
-                    "mph" -> unitMas =SpeedUnit.MPH
-                }
-                masViewModel.insert(Mas(mas, unitMas))
+//                val mas = userPrefsBinding.tiMasValue.editText?.text.toString().toDouble()
+//                var unitMas = SpeedUnit.KMH
+//                when(userPrefsBinding.tvUnitMas.text.toString()) {
+//                    "km/h" -> unitMas = SpeedUnit.KMH
+//                    "mph" -> unitMas =SpeedUnit.MPH
+//                }
+//                masViewModel.insert(Mas(mas, unitMas))
+                userPrefsViewModel.insert(UserPrefs(
+                    getMS(),
+                    getGender(),
+                    getDob(),
+                    getMasValue(), getMasUnit(),
+                    getHeightValue(), getHeightUnit(),
+                    getWeightValue(), getWeightUnit()
+                ))
             }
         }
 
@@ -174,13 +222,21 @@ class UserPrefsFragment : Fragment() {
 
         userPrefsBinding.etHeightValue.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                val height = userPrefsBinding.tiHeightValue.editText?.text.toString().toInt()
-                var unitHeight = LengthUnit.CM
-                when(userPrefsBinding.tvUnitHeight.text.toString()) {
-                    "cm" -> unitHeight = LengthUnit.CM
-                    "in" -> unitHeight = LengthUnit.IN
-                }
-                heightViewModel.insert((Height(height, unitHeight)))
+//                val height = userPrefsBinding.tiHeightValue.editText?.text.toString().toInt()
+//                var unitHeight = LengthUnit.CM
+//                when(userPrefsBinding.tvUnitHeight.text.toString()) {
+//                    "cm" -> unitHeight = LengthUnit.CM
+//                    "in" -> unitHeight = LengthUnit.IN
+//                }
+//                heightViewModel.insert((Height(height, unitHeight)))
+                userPrefsViewModel.insert(UserPrefs(
+                    getMS(),
+                    getGender(),
+                    getDob(),
+                    getMasValue(), getMasUnit(),
+                    getHeightValue(), getHeightUnit(),
+                    getWeightValue(), getWeightUnit()
+                ))
             }
         }
 
@@ -194,13 +250,21 @@ class UserPrefsFragment : Fragment() {
 
         userPrefsBinding.etWeightValue.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                val weight = userPrefsBinding.tiWeightValue.editText?.text.toString().toDouble()
-                var unitWeight = WeightUnit.KG
-                when(userPrefsBinding.tvUnitWeight.text.toString()) {
-                    "kg" -> unitWeight = WeightUnit.KG
-                    "lb" -> unitWeight = WeightUnit.LB
-                }
-                weightViewModel.insert(Weight(weight, unitWeight))
+//                val weight = userPrefsBinding.tiWeightValue.editText?.text.toString().toDouble()
+//                var unitWeight = WeightUnit.KG
+//                when(userPrefsBinding.tvUnitWeight.text.toString()) {
+//                    "kg" -> unitWeight = WeightUnit.KG
+//                    "lb" -> unitWeight = WeightUnit.LB
+//                }
+//                weightViewModel.insert(Weight(weight, unitWeight))
+                userPrefsViewModel.insert(UserPrefs(
+                    getMS(),
+                    getGender(),
+                    getDob(),
+                    getMasValue(), getMasUnit(),
+                    getHeightValue(), getHeightUnit(),
+                    getWeightValue(), getWeightUnit()
+                ))
             }
         }
 
@@ -210,5 +274,73 @@ class UserPrefsFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _userPrefsBinding = null
+    }
+
+    private fun getMS(): MeasuringSystemUnit {
+        var ms = MeasuringSystemUnit.METRIC
+        if (userPrefsBinding.rbMetric.isChecked) {
+            ms = MeasuringSystemUnit.METRIC
+        } else if (userPrefsBinding.rbImperial.isChecked) {
+            ms = MeasuringSystemUnit.IMPERIAL
+        }
+
+        return ms
+    }
+
+    private fun getGender(): GenderUnit {
+        var gender = GenderUnit.MALE
+        if (userPrefsBinding.rbMale.isChecked) {
+            gender = GenderUnit.MALE
+        } else if (userPrefsBinding.rbFemale.isChecked) {
+            gender = GenderUnit.FEMALE
+        }
+
+        return gender
+    }
+
+    private fun getDob(): Date {
+        return Date(userPrefsBinding.tvDobValue.text.toString().toLong())
+    }
+
+    private fun getMasValue(): Double {
+        return userPrefsBinding.tiMasValue.editText?.text.toString().toDouble()
+    }
+
+    private fun getMasUnit(): SpeedUnit {
+        var masUnit = SpeedUnit.KMH
+        when(userPrefsBinding.tvUnitMas.text.toString()) {
+            "km/h" -> masUnit = SpeedUnit.KMH
+            "mph" -> masUnit = SpeedUnit.MPH
+        }
+
+        return masUnit
+    }
+
+    private fun getHeightValue(): Int {
+        return userPrefsBinding.tiHeightValue.editText?.text.toString().toInt()
+    }
+
+    private fun getHeightUnit(): LengthUnit {
+        var heightUnit = LengthUnit.CM
+        when(userPrefsBinding.tvUnitHeight.text.toString()) {
+            "CM" -> heightUnit = LengthUnit.CM
+            "IN" -> heightUnit = LengthUnit.IN
+        }
+
+        return heightUnit
+    }
+
+    private fun getWeightValue(): Double {
+        return userPrefsBinding.tiWeightValue.editText?.text.toString().toDouble()
+    }
+
+    private fun getWeightUnit(): WeightUnit {
+        var weightUnit = WeightUnit.KG
+        when(userPrefsBinding.tvUnitWeight.text.toString()) {
+            "KG" -> weightUnit = WeightUnit.KG
+            "LB" -> weightUnit = WeightUnit.LB
+        }
+
+        return weightUnit
     }
 }
