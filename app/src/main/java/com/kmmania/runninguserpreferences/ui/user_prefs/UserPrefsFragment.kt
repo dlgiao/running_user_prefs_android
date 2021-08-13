28d19377bb1bf3622f5ef7914cfd4282876e0933
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-13 11:11:30 +0200

    Add weight VM

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/user_prefs/UserPrefsFragment.kt
@@ -14,10 +14,7 @@ import com.kmmania.runninguserpreferences.model.*
 import com.kmmania.runninguserpreferences.model.units.GenderUnit
 import com.kmmania.runninguserpreferences.model.units.MeasuringSystemUnit
 import com.kmmania.runninguserpreferences.ui.measuringsystem.MeasuringSystemViewModel
-import com.kmmania.runninguserpreferences.viewmodels.DobViewModel
-import com.kmmania.runninguserpreferences.viewmodels.GenderViewModel
-import com.kmmania.runninguserpreferences.viewmodels.HeightViewModel
-import com.kmmania.runninguserpreferences.viewmodels.MasViewModel
+import com.kmmania.runninguserpreferences.viewmodels.*
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
@@ -32,6 +29,7 @@ class UserPrefsFragment : Fragment() {
     private val dobViewModel: DobViewModel by viewModels()
     private val masViewModel: MasViewModel by viewModels()
     private val heightViewModel: HeightViewModel by viewModels()
+    private val weightViewModel: WeightViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
