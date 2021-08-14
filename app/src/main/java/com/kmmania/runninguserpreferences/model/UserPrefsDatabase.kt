dc4829a4b52f53c3c460795fc466f55f7e036dc5
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-14 10:42:34 +0200

    Remove @TypeConverters

--- app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefsDatabase.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/model/UserPrefsDatabase.kt
@@ -2,10 +2,8 @@ package com.kmmania.runninguserpreferences.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 
 @Database(entities = [UserPrefs::class], version = 1, exportSchema = false)
-@TypeConverters(Converters::class)
 abstract class UserPrefsDatabase: RoomDatabase() {
     abstract fun userPrefsDao(): UserPrefsDao
 }
