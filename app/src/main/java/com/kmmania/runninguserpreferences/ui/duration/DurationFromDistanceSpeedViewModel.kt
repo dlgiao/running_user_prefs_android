cbe045deef975f9024da2e8bcaad8b1c42c7d57b
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-18 16:36:15 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
@@ -1,8 +1,24 @@
 package com.kmmania.runninguserpreferences.ui.duration
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.kmmania.runninguserpreferences.model.Duration
+import com.kmmania.runninguserpreferences.model.Length
+import com.kmmania.runninguserpreferences.model.Speed
 import com.kmmania.runninguserpreferences.repositories.DurationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DurationFromDistanceSpeedViewModel(private val durationRepository: DurationRepository): ViewModel() {
+@HiltViewModel
+class DurationFromDistanceSpeedViewModel @Inject constructor(
+    private val durationRepository: DurationRepository
+): ViewModel() {
+    val duration: LiveData<Duration> = durationRepository.duration.asLiveData()
 
+    fun getDurationFromDistanceSpeed(distance: Length, speed: Speed) = viewModelScope.launch {
+        durationRepository.getDurationFromDistanceSpeed(distance, speed)
+    }
 }
