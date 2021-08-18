a2911ca950a72ac8ac9e570667090590f16d829f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-18 23:55:25 +0200

    Add UserPrefs VM

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.kmmania.runninguserpreferences.R
 import com.kmmania.runninguserpreferences.databinding.FragmentDurationFromDistanceSpeedBinding
+import com.kmmania.runninguserpreferences.model.UserPrefs
+import com.kmmania.runninguserpreferences.ui.user_prefs.UserPrefsViewModel
+import androidx.lifecycle.Observer
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DurationFromDistanceSpeedFragment : Fragment() {
     private var _durationFromDistanceSpeedBinding: FragmentDurationFromDistanceSpeedBinding? =  null
     private val durationFromDistanceSpeedBinding get() = _durationFromDistanceSpeedBinding!!
+    private val userPrefsViewModel: UserPrefsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,23 @@ class DurationFromDistanceSpeedFragment : Fragment() {
         _durationFromDistanceSpeedBinding = FragmentDurationFromDistanceSpeedBinding
             .inflate(inflater, container, false)
         val rootView = durationFromDistanceSpeedBinding.root
+
+        val userPrefsObserver = Observer<UserPrefs> { userPrefs ->
+            userPrefs?.let {
+                when(it.measuringSystem.toString()) {
+                    "METRIC" -> {
+                        durationFromDistanceSpeedBinding.tvUnitDistance.text =
+                            getString(R.string.km)
+                        durationFromDistanceSpeedBinding.tvUnitSpeed.text = getString(R.string.kmh)
+                    }
+                    "IMPERIAL" -> {
+                        durationFromDistanceSpeedBinding.tvUnitDistance.text = getString(R.string.mi)
+                        durationFromDistanceSpeedBinding.tvUnitSpeed.text = getString(R.string.mph)
+                    }
+                }
+            }
+        }
+        userPrefsViewModel.userPrefsValue.observe(viewLifecycleOwner, userPrefsObserver)
 
         return rootView
     }
