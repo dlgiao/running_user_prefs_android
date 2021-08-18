e78b4c16a6d442a82a19c7856b06ec7f5f80e5b9
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-18 16:08:38 +0200

    Add

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
@@ -0,0 +1,8 @@
+package com.kmmania.runninguserpreferences.ui.duration
+
+import androidx.lifecycle.ViewModel
+import com.kmmania.runninguserpreferences.repositories.DurationRepository
+
+class DurationFromDistanceSpeedViewModel(private val durationRepository: DurationRepository): ViewModel() {
+
+}
