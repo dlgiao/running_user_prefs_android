0a6d648bd608274fbc826a94b98130b12c635f1f
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-18 14:18:13 +0200

    Add durationBinding

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.databinding.FragmentDurationBinding
 
 class DurationFragment : Fragment() {
+    private var _durationBinding: FragmentDurationBinding? = null
+    private val durationBinding get() = _durationBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_duration, container, false)
+        _durationBinding = FragmentDurationBinding.inflate(inflater, container, false)
+        val rootView = durationBinding.root
+
+        return rootView
     }
 }
