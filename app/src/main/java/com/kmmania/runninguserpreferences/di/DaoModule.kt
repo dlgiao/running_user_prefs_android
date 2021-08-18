1968057371b3f7649a5b09a64ec83bc2ad97d025
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-18 19:11:18 +0200

    Add

---
+++ app/src/main/java/com/kmmania/runninguserpreferences/di/DaoModule.kt
@@ -0,0 +1,15 @@
+package com.kmmania.runninguserpreferences.di
+
+import com.kmmania.runninguserpreferences.model.DurationDao
+import com.kmmania.runninguserpreferences.repositories.DurationRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+abstract class DaoModule {
+    @Binds
+    abstract fun bindDurationDao(durationRepository: DurationRepository): DurationDao
+}
