004946c02d54da76161cfec35c4e121622a12b64
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-19 10:28:44 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
@@ -14,11 +14,26 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DurationFromDistanceSpeedViewModel @Inject constructor(
-    private val durationRepository: DurationRepository
+    //private val durationRepository: DurationRepository
 ): ViewModel() {
-    val duration: LiveData<Duration> = durationRepository.duration.asLiveData()
+    //val duration: LiveData<Duration> = durationRepository.duration.asLiveData()
 
-    fun getDurationFromDistanceSpeed(distance: Length, speed: Speed) = viewModelScope.launch {
-        durationRepository.getDurationFromDistanceSpeed(distance, speed)
+//    fun getDurationFromDistanceSpeed(distance: Length, speed: Speed) = viewModelScope.launch {
+//        durationRepository.getDurationFromDistanceSpeed(distance, speed)
+//    }
+
+    fun getDurationFromDistanceSpeed(distance: Length, speed: Speed): Duration {
+        val distanceValue = distance.lengthValue
+        val distanceUnit = distance.lengthUnit
+        val speedValue = speed.speedValue
+        val speedUnit = speed.speedUnit
+        val timeInSec = distanceValue?.div(speedValue!!)?.times(3600)
+
+        return Duration(
+            timeInSec?.div(3600)?.toInt(),
+            timeInSec?.rem(3600)?.div(60)?.toInt(),
+            timeInSec?.rem(3600)?.rem(60)?.toInt()!!,
+            timeInSec.minus(timeInSec.toInt()).times(1000).toInt()
+        )
     }
 }
