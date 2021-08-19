80bd66f2c1251e1d8e61adafba5866928e938ca7
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-19 16:01:48 +0200

    Update

--- app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/ui/duration/DurationFromDistanceSpeedViewModel.kt
@@ -1,20 +1,16 @@
 package com.kmmania.runninguserpreferences.ui.duration
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.kmmania.runninguserpreferences.model.Duration
 import com.kmmania.runninguserpreferences.model.Length
 import com.kmmania.runninguserpreferences.model.Speed
-import com.kmmania.runninguserpreferences.repositories.DurationRepository
+import com.kmmania.runninguserpreferences.repositories.DurationFromDistanceSpeedRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DurationFromDistanceSpeedViewModel @Inject constructor(
-    //private val durationRepository: DurationRepository
+    private val durationFromDistanceSpeedRepository: DurationFromDistanceSpeedRepository
 ): ViewModel() {
     //val duration: LiveData<Duration> = durationRepository.duration.asLiveData()
 
@@ -23,17 +19,21 @@ class DurationFromDistanceSpeedViewModel @Inject constructor(
 //    }
 
     fun getDurationFromDistanceSpeed(distance: Length, speed: Speed): Duration {
-        val distanceValue = distance.lengthValue
-        val distanceUnit = distance.lengthUnit
-        val speedValue = speed.speedValue
-        val speedUnit = speed.speedUnit
-        val timeInSec = distanceValue?.div(speedValue!!)?.times(3600)
-
-        return Duration(
-            timeInSec?.div(3600)?.toInt(),
-            timeInSec?.rem(3600)?.div(60)?.toInt(),
-            timeInSec?.rem(3600)?.rem(60)?.toInt()!!,
-            timeInSec.minus(timeInSec.toInt()).times(1000).toInt()
-        )
+        return durationFromDistanceSpeedRepository.getDuration(distance, speed)
     }
+
+//    fun getDurationFromDistanceSpeed(distance: Length, speed: Speed): Duration {
+//        val distanceValue = distance.lengthValue
+//        //val distanceUnit = distance.lengthUnit
+//        val speedValue = speed.speedValue
+//        //val speedUnit = speed.speedUnit
+//        val timeInSec = distanceValue?.div(speedValue!!)?.times(3600)
+//
+//        return Duration(
+//            timeInSec?.div(3600)?.toInt(),
+//            timeInSec?.rem(3600)?.div(60)?.toInt(),
+//            timeInSec?.rem(3600)?.rem(60)?.toInt()!!,
+//            timeInSec.minus(timeInSec.toInt()).times(1000).toInt()
+//        )
+//    }
 }
