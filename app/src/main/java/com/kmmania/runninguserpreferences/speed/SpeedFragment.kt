17521bc49cb21bb6be9be52382b522d2783fc59b
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2021-08-21 15:40:26 +0200

    Add view binding

--- app/src/main/java/com/kmmania/runninguserpreferences/speed/SpeedFragment.kt
+++ app/src/main/java/com/kmmania/runninguserpreferences/speed/SpeedFragment.kt
@@ -6,14 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kmmania.runninguserpreferences.R
+import com.kmmania.runninguserpreferences.databinding.FragmentSpeedBinding
 
 class SpeedFragment : Fragment() {
+    private var _speedFragmentBinding: FragmentSpeedBinding? = null
+    private val speedFragmentBinding get() = _speedFragmentBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_speed, container, false)
+        _speedFragmentBinding = FragmentSpeedBinding.inflate(inflater, container, false)
+        val rootView = speedFragmentBinding.root
+
+        return rootView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _speedFragmentBinding = null
     }
 }
